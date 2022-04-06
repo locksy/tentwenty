87d2f4173fb48a40bd40db0980703310b6817a38
locksy <76674794+locksy@users.noreply.github.com>
2022-04-05 22:12:05 -0700

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -9,56 +9,7 @@
  * http://www.codrops.com
  */
  
- $(function() {
-    var isMobile = window.matchMedia("only screen and (max-width: 768px)");
-    var moveForce = 30; // max popup movement in pixels
-    var rotateForce = 5; // max popup rotation in deg
-    $(".wrap").mousemove(function(e) {
-      var docX = $(this).outerWidth();
-      var docY = $(this).outerHeight();
-  
-      var moveX = (e.pageX - docX / 2) / (docX / 2) * -moveForce;
-      var moveY = (e.pageY - docY / 2) / (docY / 2) * -moveForce;
-  
-      var rotateY = (e.pageX / docX * rotateForce * 2) - rotateForce;
-      var rotateX = -((e.pageY / docY * rotateForce * 2) - rotateForce);
-  
-      $('.wrap').css({
-        '-moz-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-        '-webkit-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-        '-ms-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-        '-o-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-        'transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-      });
-    });
-    if (isMobile.matches) {
-      function onDeviceMotion(event) {
-        rotateForce = 10; // max popup rotation in deg
-        var accel = event.accelerationIncludingGravity;
-  
-        var docX = $(this).outerWidth();
-        var docY = $(this).outerHeight();
-  
-        var accelX = ((accel.x + 10) / 20) * docX;
-        var accelY = ((accel.y + 10) / 20) * docY;
-  
-        var moveX = (accelX - docX) / (docX) * -moveForce;
-        var moveY = (accelY - docY) / (docY) * -moveForce;
-  
-        var rotateY = -((accelX / docX * rotateForce * 2) - rotateForce);
-        var rotateX = -((accelY / docY * rotateForce * 2) - rotateForce);
-  
-        $('.').css({
-          '-moz-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-          '-webkit-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-          '-ms-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-          '-o-transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-          'transform': 'rotateX(' + rotateX + 'deg' + ') rotateY(' + rotateY + 'deg' + ') translateZ(0)',
-        });
-      };
-      window.addEventListener("devicemotion", onDeviceMotion, false);
-    }
-  });
+
 window.onresize = function(){ location.reload(); }
 if(window.innerHeight > window.innerWidth){
     alert("Please use landscape.  Then tilt your phone left or right to wiggle.");
