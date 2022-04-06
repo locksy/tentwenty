af2e71c8fe2feaf1d207ab40423a06c650874510
locksy <76674794+locksy@users.noreply.github.com>
2022-04-05 23:06:54 -0700

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -9,61 +9,6 @@
  * http://www.codrops.com
  */
  
-
- var orientationEl = document.getElementById('orientation'),
- accelerationEl = document.getElementById('acceleration'),
- accIncGravEl = document.getElementById('accincgrav'),
- rotationEl = document.getElementById('rotation'),
- intervalEl = document.getElementById('interval');
-
-if (window.DeviceOrientationEvent) {
- window.addEventListener('deviceorientation', deviceOrientationHandler, false)
-}
-
-function deviceOrientationHandler(evt) {
- var orientationData = evt;
- // compass direction
- orientationEl.children[1].innerHTML = evt.alpha;
-
- // vertical tilt
- orientationEl.children[3].innerHTML = evt.beta;
-
- // horizontal tilt
- orientationEl.children[5].innerHTML = evt.gamma;
-}
-
-if (window.DeviceMotionEvent) {
- window.addEventListener('devicemotion', deviceMotionHandler, false);
-}
-
-function deviceMotionHandler(evt) {
- var motionData = evt;
- // acceleration
- var evAcceleration = evt.acceleration;
-
- accelerationEl.children[1].innerHTML = evAcceleration.x;
- accelerationEl.children[3].innerHTML = evAcceleration.y;
- accelerationEl.children[5].innerHTML = evAcceleration.z;
-
- // acceleration including gravity
- var accIncGravity = evt.accelerationIncludingGravity;
-
- accIncGravEl.children[1].innerHTML = accIncGravity.x;
- accIncGravEl.children[3].innerHTML = accIncGravity.y;
- accIncGravEl.children[5].innerHTML = accIncGravity.z;
-
- // rotation rate
- var rotationRate = evt.rotationRate;
-
- rotationEl.children[1].innerHTML = rotationRate.alpha;
- rotationEl.children[3].innerHTML = rotationRate.beta;
- rotationEl.children[5].innerHTML = rotationRate.gamma;
-
- // interval
- var interval = evt.interval;
- intervalEl.children[1].innerHTML = interval;
-}
-
 window.onresize = function(){ location.reload(); }
 if(window.innerHeight > window.innerWidth){
     alert("Please use landscape.  Then tilt your phone left or right to wiggle.");
