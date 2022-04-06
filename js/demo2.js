04c63c0e808669f6db6010d2bd81be2a71ef3d9a
locksy <76674794+locksy@users.noreply.github.com>
2022-04-05 23:19:46 -0700

    Update demo2.js
    d

--- js/demo2.js
+++ js/demo2.js
@@ -10,6 +10,7 @@
  */
  
 
+ 
 
 window.onresize = function(){ location.reload(); }
 if(window.innerHeight > window.innerWidth){
@@ -78,9 +79,9 @@ if(window.innerHeight > window.innerWidth){
 
         function myFunction(x) {
             if (x.matches) { // If media query matches
-              createBlotterText.size = 10;
+              createBlotterText.size = 60;
             } 
-          var x = window.innerWidth > 768 ? 150 : 10;
+          var x = window.innerWidth > 768 ? 150 : 60;
           myFunction(x) // Call listener function at run time
           x.addListener(myFunction) // Attach listener function on state changes
         };
@@ -100,7 +101,7 @@ if(window.innerHeight > window.innerWidth){
 
         const render = () => {
             const docScrolls = {left : body.scrollLeft + docEl.scrollLeft, top : body.scrollTop + docEl.scrollTop};
-            const relmousepos = {x : alpha - docScrolls.left, y : beta - docScrolls.top };
+            const relmousepos = {x : mousePos.x - docScrolls.left, y : mousePos.y - docScrolls.top };
             const mouseDistance = MathUtils.distance(lastMousePosition.x, relmousepos.x, lastMousePosition.y, relmousepos.y);
 
             volatility = MathUtils.lerp(volatility, Math.min(MathUtils.lineEq(0.9, 0, 100, 0, mouseDistance),0.9), 0.05);
